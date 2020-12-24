454f5aa0a289e6e0caf25f4f5e31f9a3e1fe7f30
Davor Ocelic <docelic@crystallabs.io>
2020-12-24 17:51:47 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,39 @@
+# crystallabs-helpers
+
+TODO: Write a description here
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     crystallabs-helpers:
+       github: your-github-user/crystallabs-helpers
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "crystallabs-helpers"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/crystallabs-helpers/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [your-name-here](https://github.com/your-github-user) - creator and maintainer
---
+++ src/crystallabs-helpers.cr
@@ -0,0 +1,85 @@
+module Crystallabs::Helpers
+  VERSION = "0.1.0"
+
+  module Object_Inspect
+    def i; inspect end
+  end
+
+  module Logging
+    macro included
+      Log = ::Log.for self.name
+    end
+  end
+
+  module Boolean
+
+    # :nodoc:
+    def to_b(arg : String, empty = false)
+      return empty if !empty.nil? && (arg.nil? || arg.empty?)
+      return false if {"false", "", "0", "-0", "0n", " "}.includes? arg
+      true
+    end
+
+    # :nodoc:
+    def to_b(arg : Int, empty = false)
+      arg != 0
+    end
+
+    # :nodoc:
+    def to_b(arg : Char, empty = false)
+      to_b arg.to_s
+    end
+
+    def to_b(arg : Nil, empty = false)
+      false
+    end
+
+    def to_b(arg : Bool)
+      arg
+    end
+  end
+
+  module File
+    # TODO try_read(Str | Arr), return first found, else ''
+  end
+
+  module Alias_Methods
+    # Defines new_method as an alias of old_method.
+    #
+    # This creates a new method new_method that invokes old_method.
+    #
+    # Note that due to current language limitations this is only useful
+    # when neither named arguments nor blocks are involved.
+    #
+    # ```
+    # class Person
+    #   getter name
+    #
+    #   def initialize(@name)
+    #   end
+    #
+    #   alias_method full_name, name
+    # end
+    #
+    # person = Person.new "John"
+    # person.name #=> "John"
+    # person.full_name #=> "John"
+    # ```
+    #
+    # This macro was present in Crystal until commit 7c3239ee505e07544ec372839efed527801d210a.
+    macro alias_method(new_method, old_method)
+      def {{new_method.id}}(*args)
+        {{old_method.id}}(*args)
+      end
+    end
+
+    # Defines new_method as an alias of last (most recently defined) method.
+    macro alias_previous(*new_methods)
+      {% for new_method in new_methods %}
+        alias_method new_method, {{@type.methods.last.name}}
+        #p "{{new_method}} -> {{@type.methods.last.name}}"
+      {% end %}
+    end
+  end
+
+end
